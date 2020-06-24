a5b4851822c4c00e1cbb9be33bd171af767edc6d
rmah207 <67291626+rmah207@users.noreply.github.com>
2020-06-24 11:48:23 -0600

    Fixed column names

--- generate_dl_json.py
+++ generate_dl_json.py
@@ -6,7 +6,6 @@ from collections import OrderedDict
 # To run, you will have to do "pip3 install numpy" and "pip3 install pandas"
 
 def filter_nan(val):
-    """Filters out NaNs cus they're stupid"""
     if (val is np.NaN) or (np.isnan(val)):
         return ""
     else:
@@ -14,12 +13,12 @@ def filter_nan(val):
 
 def add_field_info(
     json_dict, category, group, field,
+    port,
     header,
     data_size,
     bit_start,
     bit_end,
     datatype,
-    round_val,
     multiplier):
     """Writes the field info into json_dict"""
 
@@ -30,20 +29,20 @@ def add_field_info(
             "bit_start" : str(bit_start),
             "bit_end" : str(bit_end),
             "type" : str(datatype),
-            "round" : str(filter_nan(round_val)),
             "multiplier" : str(filter_nan(multiplier))
         }        
+        json_dict[category][group]["port"] = str(port)
         json_dict[category][group]["header"] = str(header)
         json_dict[category][group][field] = temp
     
     else:
         temp = {
+            "port" : str(port),
             "header" : str(header),
             "data_size" : str(data_size),
             "bit_start" : str(bit_start),
             "bit_end" : str(bit_end),
             "type" : str(datatype),
-            "round" : str(filter_nan(round_val)),
             "multiplier" : str(filter_nan(multiplier))
         }        
         json_dict[category][field] = temp
@@ -51,22 +50,25 @@ def add_field_info(
 
 # There's definitely a better way of doing this, but oh well
 
+#################################################################################################################################
+# Replace with the appropriate directories:
+csv_path = r"C:\Users\rmah\VS-Code\Encoder-Decoder-JSON-Generator\DL\resources\HomeSensorFieldsDL.csv"
+json_path = r"C:\Users\rmah\VS-Code\Encoder-Decoder-JSON-Generator\DL\downlinkHomeSensor.json"
+#################################################################################################################################
 
-# Replace with your own directory
-df = pd.read_csv("C:\\Users\\rmah\\VS-Code\\Encoder-Decoder-JSON-Generator\\DL\\resources\\homeSensor.csv")
-# print(len(df))
 
+df = pd.read_csv(csv_path)
 json_dict = OrderedDict()
 
 prev_cat = ""   # cat -> category
-current_cat = df["category_name"][0]
+current_cat = df["Category name"][0]
 
 if (not pd.isnull(current_cat)) and (current_cat != "") and (current_cat is not np.NaN):
     json_dict[current_cat] = OrderedDict()
 
 for i in range(1, len(df)):
     prev_cat = current_cat
-    current_cat = df["category_name"][i]
+    current_cat = df["Category name"][i]
 
     if prev_cat != current_cat:
         json_dict[current_cat] = OrderedDict()
@@ -74,19 +76,19 @@ for i in range(1, len(df)):
 #################################################################################
 
 prev_cat = ""
-current_cat = df["category_name"][0]
+current_cat = df["Category name"][0]
 
 prev_group = ""
-current_group = df["group_name"][0]
+current_group = df["Group name"][0]
 if (not pd.isnull(current_group)) and (current_group != "") and (current_group is not np.NaN):
     json_dict[current_cat][current_group] = OrderedDict()
 
 for i in range(1, len(df)):
     prev_cat = current_cat
-    current_cat = df["category_name"][i]
+    current_cat = df["Category name"][i]
 
     prev_group = current_group
-    current_group = df["group_name"][i]
+    current_group = df["Group name"][i]
 
     if (prev_group != current_group) and (current_group is not np.NaN):
         json_dict[current_cat][current_group] = OrderedDict()
@@ -94,36 +96,52 @@ for i in range(1, len(df)):
 #################################################################################
 
 prev_cat = ""
-current_cat = df["category_name"][0]
+current_cat = df["Category name"][0]
 prev_group = ""
-current_group = df["group_name"][0]
+current_group = df["Group name"][0]
+
+port = df["Port"][0]
+header = df["Header"][0]
+data_size = df["Data size"][0]
+bit_start = df["Bit start"][0]
+bit_end = df["Bit end"][0]
+datatype = df["Type"][0]
+multiplier = df["Multiplier"][0]
+add_field_info(
+    json_dict, current_cat, current_group, df["Field name"][0],
+    port,
+    header,
+    data_size,
+    bit_start,
+    bit_end,
+    datatype,
+    multiplier)
 
 for i in range(1, len(df)):
     prev_cat = current_cat
-    current_cat = df["category_name"][i]
+    current_cat = df["Category name"][i]
     prev_group = current_group
-    current_group = df["group_name"][i]
+    current_group = df["Group name"][i]
 
-    header = df["header"][i]
-    data_size = df["data_size"][i]
-    bit_start = df["bit_start"][i]
-    bit_end = df["bit_end"][i]
-    datatype = df["type"][i]
-    round_val = df["round"][i]
-    multiplier = df["multiplier"][i]
+    port = df["Port"][i]
+    header = df["Header"][i]
+    data_size = df["Data size"][i]
+    bit_start = df["Bit start"][i]
+    bit_end = df["Bit end"][i]
+    datatype = df["Type"][i]
+    multiplier = df["Multiplier"][i]
 
     add_field_info(
-        json_dict, current_cat, current_group, df["parameter_name"][i],
+        json_dict, current_cat, current_group, df["Field name"][i],
+        port,
         header,
         data_size,
         bit_start,
         bit_end,
         datatype,
-        round_val,
         multiplier)
-    
-# Replace with your appropriate directory
-with open("C:\\Users\\rmah\\VS-Code\\Encoder-Decoder-JSON-Generator\\DL\\downlinkHomeSensor.json", "w") as f:
+
+with open(json_path, "w") as f:
     json.dump(json_dict, f, indent = 4)
 
 
